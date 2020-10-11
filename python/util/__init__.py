9396f3091dd34c186ac1c1c97bc8a7528d83a45c
Hepheir <hepheir@gmail.com>
2020-10-12 03:23:13 +0900

    Create util.default()

--- python/util/__init__.py
+++ python/util/__init__.py
@@ -1,3 +1,4 @@
 from . import datatypes
 from . import exceptions
 
+from .default import default
---
+++ python/util/default.py
@@ -0,0 +1,2 @@
+def default(value, default_value):
+    return value if value is not None else default_value
