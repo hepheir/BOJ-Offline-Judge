e036287f3dc703029f0f7df57c0f3e281d91ad00
Hepheir <hepheir@gmail.com>
2021-03-04 12:49:34 +0900

    Create config.json if does not exist

--- boj/config.py
+++ boj/config.py
@@ -23,6 +23,24 @@ def from_json(key):
 # 설정 불러오기
 ###############################################################
 
-with open(__CONFIG_FILE, 'r') as f:
-    __CONFIG = json.loads(f.read())
+
+if not os.path.exists(__CONFIG_FILE):
+    # DEFAULT CONFIGURATION
+    __CONFIG = {
+        "data.in.path.pattern": "{srcDirname}\\data\\**\\*.in",
+        "data.out.path.pattern": "{srcDirname}\\data\\**\\*.out",
+
+        "timelimit.ms": 10000,
+
+        "result.include.time": True,
+        "result.include.datapath": True,
+
+        "cpp.compiler.path": "c:\\MinGW\\bin\\g++.exe",
+        "c.compiler.path": "c:\\MinGW\\bin\\gcc.exe"
+    }
+    with open(__CONFIG_FILE, 'w') as f:
+        f.write(json.dumps(__CONFIG))
+else:
+    with open(__CONFIG_FILE, 'r') as f:
+        __CONFIG = json.loads(f.read())
 
