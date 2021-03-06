232a4d1cdba05c885ff9f351393eba6e4b9ccc5b
Hepheir <hepheir@gmail.com>
2021-03-06 23:31:49 +0900

    `load_testcases()` no more detects language and make temporary source backups

--- boj/judge/Testcase.py
+++ boj/judge/Testcase.py
@@ -1,9 +1,9 @@
-import pathlib
 import glob
+import pathlib
+import typing
 
-from boj.lang import BaseLanguage, detect_language
+from boj.lang import BaseLanguage
 from boj.config import config
-from boj.util.path import temp_makecopy
 from boj.util.format import format_src
 
 
@@ -15,9 +15,7 @@ class TestCase:
         self.language = language
 
 
-def load_testcases(src: pathlib.Path):
-    language = detect_language(src)
-    source_file = temp_makecopy(src)
+def load_testcases(source_file: pathlib.Path, language:BaseLanguage) -> typing.Generator[TestCase]:
     input_files = glob.glob(format_src(config.get('Paths', 'inputfilenamePatterns')), recursive=True)
     output_files = glob.glob(format_src(config.get('Paths', 'outputfilenamePatterns')), recursive=True)
     for input_file, output_file in zip(input_files, output_files):
