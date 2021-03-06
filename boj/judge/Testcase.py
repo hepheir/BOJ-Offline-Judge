f7f20cc086a6760f042aedee54bb759e9bb1fbc1
Hepheir <hepheir@gmail.com>
2021-03-06 23:41:43 +0900

    Modularize `load_testcases()`

--- boj/judge/Testcase.py
+++ boj/judge/Testcase.py
@@ -1,10 +1,6 @@
-import glob
 import pathlib
-import typing
 
 from boj.lang import BaseLanguage
-from boj.config import config
-from boj.util.format import format_src
 
 
 class TestCase:
@@ -13,10 +9,3 @@ class TestCase:
         self.input_file = input_file
         self.output_file = output_file
         self.language = language
-
-
-def load_testcases(source_file: pathlib.Path, language:BaseLanguage) -> typing.Generator[TestCase]:
-    input_files = glob.glob(format_src(config.get('Paths', 'inputfilenamePatterns')), recursive=True)
-    output_files = glob.glob(format_src(config.get('Paths', 'outputfilenamePatterns')), recursive=True)
-    for input_file, output_file in zip(input_files, output_files):
-        yield TestCase(source_file, input_file, output_file, language)
