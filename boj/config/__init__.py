ed901f31bcfcc42dba6b2822fa34b98ed9edb867
김동주 <hepheir@gmail.com>
2021-03-12 11:29:45 +0900

    어색한 표현 수정 & autopep8

--- boj/config/__init__.py
+++ boj/config/__init__.py
@@ -11,28 +11,31 @@ from boj.__version__ import __version__
 
 CONFIG_FILE = os.path.join(SETTINGS_DIRECTORY, 'config.ini')
 
-print('[INFO]',f'설정 파일 "{CONFIG_FILE}"')
+print('[INFO]', f'설정 파일 "{CONFIG_FILE}"')
 print('----------------------------------------------------------------')
 
 config = configparser.ConfigParser()
 
 if os.path.isfile(CONFIG_FILE):
-    print('[INFO]', '설정 파일을 불러오고 있습니다...')
+    print('[INFO]', '설정 파일을 발견하였습니다.')
     config.read(CONFIG_FILE, encoding='utf-8')
-    
+
     if config.get('user', 'version', fallback='<no_version>') != __version__:
-        print('[INFO]', '설정 파일을 업데이트 합니다.')
+        print('[INFO]', '설정 파일을 현재의 버전에 맞도록 업데이트 합니다.')
         config['DEFAULT'] = DEFAULT_SETTINGS
         # 기존 사용자 설정은 유지하며 새로운 항목 업데이트
         for option, default_value in USER_SETTINGS.items():
-            config['user'][option] = config.get('user', option, fallback=default_value)
+            config['user'][option] = config.get(
+                'user', option, fallback=default_value)
         # 설정 파일 생성
         with open(CONFIG_FILE, 'w') as config_file:
             config.write(config_file)
         print('[INFO]', '설정 파일 업데이트를 완료했습니다.')
-          
+    else:
+        print('[INFO]', '설정 파일을 불러왔습니다.')
+
 elif os.path.isdir(CONFIG_FILE):
-    raise IsADirectoryError(f'파일 생성 실패.\n같은 명의 디렉토리가 존재합니다.')
+    raise IsADirectoryError('설정 파일을 생성 할 수 없습니다.\n같은 명의 디렉토리가 존재합니다.')
 
 else:
     print('[INFO]', '설정 파일을 생성하고 있습니다...')
@@ -42,7 +45,5 @@ else:
     with open(CONFIG_FILE, 'w') as config_file:
         config.write(config_file)
     print('[INFO]', '설정 파일 생성을 완료했습니다.')
-
-print('[INFO]',f'설정 파일을 업데이트 했습니다.')
 
 print('================================================================')
--- boj/config/settings_directory.py
+++ boj/config/settings_directory.py
@@ -5,22 +5,22 @@ SETTINGS_DIRECTORY = os.path.join(os.getcwd(), '.boj')
 
 ###############################################################
 
-print('[INFO]',f'설정 디렉토리: "{SETTINGS_DIRECTORY}"')
+print('[INFO]', f'설정 디렉토리: "{SETTINGS_DIRECTORY}"')
 print('----------------------------------------------------------------')
 
 if os.path.isdir(SETTINGS_DIRECTORY):
-    print('[INFO]', '설정 디렉토리를 읽어오고 있습니다...')
+    print('[INFO]', '설정 디렉토리를 발견하였습니다.')
 
 elif not os.path.exists(SETTINGS_DIRECTORY):
     print('[INFO]', '설정 디렉토리를 생성하고 있습니다...')
 
     os.makedirs(SETTINGS_DIRECTORY)
-    
+
     # .gitignore 생성
     with open(os.path.join(SETTINGS_DIRECTORY, '.gitignore'), 'w') as gitignore_file:
         gitignore_file.write('*\n')
-    
-    print('[INFO]',f'설정 디렉토리를 생성하였습니다.')
+
+    print('[INFO]', f'설정 디렉토리를 생성하였습니다.')
 
 else:
     raise FileExistsError(f'"{SETTINGS_DIRECTORY}"가 존재하여 설정 디렉토리를 생성할 수 없습니다.')
