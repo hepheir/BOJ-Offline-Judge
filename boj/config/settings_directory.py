756a20b4a7630bbc86c65f6cf03bac1f970230c9
김동주 <hepheir@gmail.com>
2021-03-12 16:04:37 +0900

    f-string is missing placeholders (F541)

--- boj/config/settings_directory.py
+++ boj/config/settings_directory.py
@@ -20,7 +20,7 @@ elif not os.path.exists(SETTINGS_DIRECTORY):
     with open(os.path.join(SETTINGS_DIRECTORY, '.gitignore'), 'w') as gitignore_file:
         gitignore_file.write('*\n')
 
-    print('[INFO]', f'설정 디렉토리를 생성하였습니다.')
+    print('[INFO]', '설정 디렉토리를 생성하였습니다.')
 
 else:
     raise FileExistsError(f'"{SETTINGS_DIRECTORY}"가 존재하여 설정 디렉토리를 생성할 수 없습니다.')
