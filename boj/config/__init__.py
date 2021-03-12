ead0739ab64719d955629ff56036c919a521259e
김동주 <hepheir@gmail.com>
2021-03-12 14:19:53 +0900

    config.ini에서 utf-8 문자가 깨지던 문제 해결

--- boj/config/__init__.py
+++ boj/config/__init__.py
@@ -1,3 +1,4 @@
+import codecs
 import configparser
 import os
 
@@ -6,30 +7,33 @@ from boj.config.default_settings import DEFAULT_SETTINGS
 from boj.config.user_settings import USER_SETTINGS
 from boj.__version__ import __version__
 
+CONFIG_FILE = os.path.join(SETTINGS_DIRECTORY, 'config.ini')
+
 
 ###############################################################
 
-CONFIG_FILE = os.path.join(SETTINGS_DIRECTORY, 'config.ini')
+def _save_as(filename, *args, **kwargs):
+    with open(filename, 'w', encoding='utf8') as config_file:
+        config.write(config_file)
+
+
+config = configparser.ConfigParser()
+config.save_as = _save_as
 
 print('[INFO]', f'설정 파일 "{CONFIG_FILE}"')
 print('----------------------------------------------------------------')
 
-config = configparser.ConfigParser()
-
 if os.path.isfile(CONFIG_FILE):
     print('[INFO]', '설정 파일을 발견하였습니다.')
-    config.read(CONFIG_FILE, encoding='utf-8')
+    config.read_file(codecs.open(CONFIG_FILE, "r", "utf8"))
 
     if config.get('user', 'version', fallback='<no_version>') != __version__:
         print('[INFO]', '설정 파일을 현재의 버전에 맞도록 업데이트 합니다.')
         config['DEFAULT'] = DEFAULT_SETTINGS
         # 기존 사용자 설정은 유지하며 새로운 항목 업데이트
         for option, default_value in USER_SETTINGS.items():
-            config['user'][option] = config.get(
-                'user', option, fallback=default_value)
-        # 설정 파일 생성
-        with open(CONFIG_FILE, 'w') as config_file:
-            config.write(config_file)
+            config['user'][option] = config.get('user', option, fallback=default_value)
+        config.save_as(CONFIG_FILE)
         print('[INFO]', '설정 파일 업데이트를 완료했습니다.')
     else:
         print('[INFO]', '설정 파일을 불러왔습니다.')
@@ -41,9 +45,7 @@ else:
     print('[INFO]', '설정 파일을 생성하고 있습니다...')
     config['DEFAULT'] = DEFAULT_SETTINGS
     config['user'] = USER_SETTINGS
-    # 설정 파일 생성
-    with open(CONFIG_FILE, 'w') as config_file:
-        config.write(config_file)
+    config.save_as(CONFIG_FILE)
     print('[INFO]', '설정 파일 생성을 완료했습니다.')
 
 print('================================================================')
