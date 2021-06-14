ad4edcab44bf782c405f55fcc02d243638aaf989
김동주 <hepheir@gmail.com>
2021-06-14 14:44:26 +0900

    Create template method of `Problem.load()`

--- boj/python/core/problem.py
+++ boj/python/core/problem.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import dataclasses
-from typing import Callable
+from typing import Callable, Optional, overload
 
 from boj.python.core.web import HTMLElement
 
@@ -20,16 +20,67 @@ class Rule:
     DEFAULT = 1
     SPECIAL_JUDGE = 2
 
-    judge_method: int
-    time_limit: float
-    memory_limit: float
+    judge_method: int = dataclasses.field(default=None)
+    time_limit: float = dataclasses.field(default=None)
+    memory_limit: float = dataclasses.field(default=None)
 
-
+@dataclasses.dataclass
 class Problem:
-    number: int
-    title: str
-    document: Document
-    rule: Rule
+    def __init__(self) -> None:
+        self.number: Optional[int] = None
+        self.title: Optional[str] = None
+        self.document: Document = Document()
+        self.rule: Rule = Rule()
 
     save: Callable[[str], None]
-    load: Callable[[str], None]
+
+    @overload
+    def load(self) -> None:
+        """문제 정보를 백준 온라인 저지로 부터 불러옵니다.
+
+        문제를 불러오기 위해서는 문제의 번호가 반드시 주어져야 합니다.
+        문제의 번호가 주어지지 않은 상태로 불러오기를 시도할 경우 `AssertionError`를 발생시킵니다.
+
+        Raises:
+            AssertionError: 문제 번호가 올바르지 않거나 존재하지 않으면 발생.
+        """
+        ...
+
+    @overload
+    def load(self, path: str) -> None:
+        """문제 정보를 `path`에 해당하는 경로로 부터 불러옵니다.
+
+        문제 정보는 반드시 다음의 형식에 맞게 저장되어 있어야 합니다.
+        ```text
+            path/
+            ├─ info.json
+            ├─ rule.json
+            ├─ document/
+            │  ├─ desc.html
+            │  ├─ input.html
+            │  ├─ output.html
+            │  └─ hint.html
+            └─ data/
+                ├─ sample-1.in
+                ├─ sample-1.out
+                └─ ...
+        ```
+
+        Raises:
+            FileNotFoundError: `path`로 부터 파일을 찾지 못하였을 때 발생.
+        """
+        ...
+
+    def load(self, path: Optional[str] = None) -> None:
+        if path is None:
+            return self._load_from_web()
+        else:
+            return self._load_from_local(path)
+
+    def _load_from_web(self) -> None:
+        # TODO
+        pass
+
+    def _load_from_local(self, path: str) -> None:
+        # TODO
+        pass
