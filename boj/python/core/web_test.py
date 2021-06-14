070c3ba3e38e5dc9a0b773d20d4fb239943d8674
김동주 <hepheir@gmail.com>
2021-06-14 10:49:50 +0900

    Update unittest for `Crawler.get()`

--- boj/python/core/web_test.py
+++ boj/python/core/web_test.py
@@ -2,12 +2,13 @@ import unittest
 from boj.python.core.web import Crawler
 
 
-class test_get(unittest.TestCase):
+class Test_Crawler(unittest.TestCase):
     maxDiff = None
 
-    def testGetMethod(self):
-        url = 'https://github.com/Hepheir/BOJ-Offline-Judge/files/6643645/TESTING.txt'
-        html = Crawler.get(url)
-        self.assertTrue(isinstance(html, str))
-        self.assertEqual(html, 'Test for boj.python.core.web.Crawler.get()')
-        self.assertNotEqual(html, ' ')
+    def testGet_TestFile(self):
+        TEST_URL = 'https://github.com/Hepheir/BOJ-Offline-Judge/files/6643645/TESTING.txt'
+        TEST_HTML = 'Test for boj.python.core.web.Crawler.get()'
+        html = Crawler.get(TEST_URL)
+        self.assertTrue(isinstance(html, str),
+                        f'`Crawler.get()` should return `str` type, not {type(html)}')
+        self.assertEqual(html, TEST_HTML)
